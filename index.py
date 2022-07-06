5a7e9dbfb8df962ddcc976b48fcbe1f98a8bf354
TahlonBrahic <104690672+TahlonBrahic@users.noreply.github.com>
2022-07-06 08:32:25 -0500

    Add files via upload
    add tab icon

--- index.py
+++ index.py
@@ -6,12 +6,14 @@ import random
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption('PyPals')
+icon = pygame.image.load('Projects\PyPals\\resources\icon\\turtle.png')
+pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 # test_font = pygame.font.Font('X:\Files\Programming\Projects\PyPals\resources\font\Pixeltype.ttf', 50)
 game_active = False
 start_time = 0
 
-# create game loop
+# game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
