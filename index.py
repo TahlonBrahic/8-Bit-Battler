24ad786c4018862b220f22ff2c600d6a8a04fb8b
TahlonBrahic <104690672+TahlonBrahic@users.noreply.github.com>
2023-01-21 13:16:06 -0600

    Add files via upload

--- index.py
+++ index.py
@@ -13,17 +13,22 @@ pygame.font.Font('X:\\Files\\Programming\\Projects\\PyPals\\resources\\font\\Pix
 game_active = False
 start_time = 0
 
-# player
+# player 1
 playerImage1 = pygame.image.load('X:\Files\Programming\Projects\PyPals\\resources\icon\\toucan.png')
-playerX = 800
-playerY = 200
+player1X = 800
+player1Y = 200
 fireball_attack = pygame.image.load('X:\Files\Programming\Projects\PyPals\\resources\icon\\fireball\efecto_fuego_00011.png')
+
+# player 2
+playerImage2 = pygame.image.load('X:\Files\Programming\Projects\PyPals\\resources\icon\\turtle.png')
+player2X = 200
+player2Y = 200
 
 def player1(x,y):
     screen.blit(playerImage1, (x, y))
 
 def player2(x,y):
-    screen.blit(playerImage, (x,y))
+    screen.blit(playerImage2, (x,y))
 
 # game loop
 while True:
@@ -35,26 +40,42 @@ while True:
             pygame.quit()
             exit()
 
-        # player movement
+        # player1 movement
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX -= 10
+                player1X -= 10
             if event.key == pygame.K_RIGHT:
-                playerX += 10
+                player1X += 10
             if event.key == pygame.K_UP:
-                playerY -= 10
+                player1Y -= 10
             if event.key == pygame.K_DOWN:
-                playerY += 10
+                player1Y += 10
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                pass
         
-        # player attack
+        # player1 attack
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_f:
                 screen.blit(fireball_attack, (100, 100))
 
+        # player2 movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player2X -= 10
+            if event.key == pygame.K_d:
+                player2X += 10
+            if event.key == pygame.K_w:
+                player2Y -= 10
+            if event.key == pygame.K_d:
+                player2Y += 10
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+               pass
+        
+
 
     
-    player1(playerX, playerY)
+    player1(player1X, player1Y)
+    player2(player2X, player2Y)
     pygame.display.update()
